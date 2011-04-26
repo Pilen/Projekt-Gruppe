66faa443b573e76b2400aa90e4b69d8e65b53c51
Søren Pilgård <fiskomaten@gmail.com>
2011-04-26 12:34:16 +0200

    din mor

--- spil0/config.py
+++ spil0/config.py
@@ -2,7 +2,6 @@
 import pygame
 import resources, classes
 
-
 entities = pygame.sprite.RenderUpdates()
 walls    = pygame.sprite.Group()
 monsters = pygame.sprite.Group()
@@ -12,10 +11,12 @@ player   = None
 background, background_rect= resources.load_image("background2.png")
 level = resources.load_map("map0.txt")
 
+def proccesCharacter(x,y):
+    if level[y][x] == '#':
+        classes.Wall(x*32,y*32)
+    elif level[y][x] == '@':
+        classes.Player(x*32,y*32)
+
 for y in range(0,(600/32)):
     for x in range(0,(800/32)):
-        if level[y][x] == '#':
-            classes.Wall(x*32,y*32)
-        elif level[y][x] == '@':
-            classes.Player(x*32,y*32)
-
+        proccesCharacter(x,y)
